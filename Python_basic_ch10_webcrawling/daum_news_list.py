8381e250b18ae4d5ac9fb61d36027eae8750f654
NaGeon87 <nageon87@gmail.com>
2023-05-16 09:46:22 +0900

    Add daum_news_page

--- Python_basic_ch10_webcrawling/daum_news_list.py
+++ Python_basic_ch10_webcrawling/daum_news_list.py
@@ -2,11 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 from daum_news_fnc import get_news_title_and_content
 
-url = "https://v.daum.net/v/20230509090530015"
+url = "https://news.daum.net/breakingnews/digital"
 
 result = requests.get(url)
 soup = BeautifulSoup(result.text,"html.parser")
-title_list = soup.select("ul.list_news2a.link_txt")
+title_list = soup.select("ul.list_news2 a.link_txt")
+
 for i, tag in enumerate(title_list):
     new_url = tag["href"]
     title, content = get_news_title_and_content(new_url)
